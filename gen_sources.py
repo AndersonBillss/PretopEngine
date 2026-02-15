6035db02a545dbdaad06d04a39bbada2bf9f16dd
AndersonBillss <anderson@thebillsfamily.com>
2026-02-15 01:30:35 -0700

    Dynamically add targets based on file prefix

--- gen_sources.py
+++ gen_sources.py
@@ -3,11 +3,39 @@ import os
 
 SRC_DIR = "src"
 OUT_FILE = "sources.cmake"
-WEB_PREFIX = "WEB_"
-NATIVE_PREFIX = "NATIVE_"
+STARTING_COMMENT = "# Auto-generated code, do not edit"
+RULE_PREFIX = "AB_ENGINE_SOURCES"
 
 
-def generate_set(sources, label) -> str:
+class SourceTracker:
+    def __init__(self):
+        self.src_files = []
+        self.src_targets = {}
+
+    def add_file(self, filepath):
+        has_underscore = filepath.find("_") != -1
+        if has_underscore:
+            # Grab the file/directory that has an underscore. Whatever is before the underscore will be the rule
+            split_filepath_underscore = filepath.split("_")
+            until_underscore = "_".join(split_filepath_underscore[:-1])
+            split_slash = until_underscore.split("/")
+            target_name = split_slash[-1]
+            self._add_target_file(target_name, filepath)
+
+        else:
+            self._add_src_file(filepath)
+
+    def _add_src_file(self, filepath):
+        self.src_files += [filepath]
+
+    def _add_target_file(self, target, filepath):
+        if target in self.src_targets:
+            self.src_targets[target] += [filepath]
+        else:
+            self.src_targets[target] = [filepath]
+
+
+def generate_rule(sources, label) -> str:
     concat_files = ""
     for i, filepath in enumerate(sources):
         if i != 0:
@@ -20,35 +48,23 @@ def generate_set(sources, label) -> str:
 
 
 def gen_sources():
-    src_files = []
-    web_specific_src_files = []
-    native_specific_src_files = []
+    tracker = SourceTracker()
     for root, dirs, files in os.walk("src"):
         for file in files:
+            filepath = os.path.join(root, file).replace("\\", "/")
             if file.endswith(".cpp"):
-                filepath = os.path.join(root, file).replace("\\", "/")
-                if file.startswith(WEB_PREFIX):
-                    web_specific_src_files += [filepath]
-                elif file.startswith(NATIVE_PREFIX):
-                    native_specific_src_files += [filepath]
-                else:
-                    src_files += [filepath]
+                tracker.add_file(filepath)
 
-    src_files_str = generate_set(src_files, "AB_ENGINE_SOURCES")
+    src_files_rule = generate_rule(tracker.src_files, RULE_PREFIX)
+    target_files_rules = [
+        generate_rule(tracker.src_targets[key], f"{RULE_PREFIX}_" + key)
+        for key in tracker.src_targets
+    ]
 
-    native_files_str = generate_set(
-        native_specific_src_files, "AB_ENGINE_SOURCES_NATIVE"
-    )
-
-    web_specific_src_files = generate_set(
-        web_specific_src_files, "AB_ENGINE_SOURCES_WEB"
-    )
-
-    file_content = f"""# Auto-generated code, do not edit
-{src_files_str}
-{native_files_str}
-{web_specific_src_files}
-"""
+    file_content = STARTING_COMMENT
+    file_content += f"\n{src_files_rule}\n"
+    for rule in target_files_rules:
+        file_content += f"{rule}\n"
 
     with open(OUT_FILE, "w") as output:
         output.write(file_content)
