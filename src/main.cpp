e729072c8e9d3489b1c9bd27e99517fb53957fe2
AndersonBillss <anderson@thebillsfamily.com>
2026-02-25 23:24:02 -0700

    Removed commented code

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,6 @@
 #include "window/windowFactory.hpp"
 #include "application.hpp"
 
-// void onDeviceLost(WGPUDevice const *device, WGPUDeviceLostReason reason, WGPUStringView message, void *, void *)
-// {
-//   if (reason != WGPUDeviceLostReason::WGPUDeviceLostReason_Destroyed)
-//     std::cout << "WGPU device lost: " << message << std::endl;
-// }
-
-// void onDeviceUncapturedError(WGPUDevice const *device, WGPUErrorType type, WGPUStringView message, void *, void *)
-// {
-//   std::cout << "WGPU device error: " << message << std::endl;
-// }
-
 int main(int, char **)
 {
   std::cout << "Hello, WebGPU!!" << std::endl;
