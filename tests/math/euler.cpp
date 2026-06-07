4e2e8002a75f68b4ca64c26d5ec79dad5ebae3c9
AndersonBillss <anderson@thebillsfamily.com>
2026-06-06 19:23:21 -0600

    Added more tests for Eulers

--- tests/math/euler.cpp
+++ tests/math/euler.cpp
@@ -3,7 +3,7 @@
 #include "../../src/math/euler.hpp"
 #include "../../src/math/constants.hpp"
 
-TEST_CASE("Roll euler rotate basis vectors as expected", "[math][euler]")
+TEST_CASE("X euler rotate basis vectors as expected", "[math][euler]")
 {
     Euler e{90.0f * (float)deg2rad, 0, 0};
     Vec4 v1{1, 0, 0, 0};
@@ -16,5 +16,37 @@ TEST_CASE("Roll euler rotate basis vectors as expected", "[math][euler]")
 
     Vec4 v3{0, 0, 1, 0};
     Vec4 result3{0, -1, 0, 0};
+    REQUIRE_THAT(e.toMatrix() * v3, Vec4ApproxMatcher(result3, 1e-5f));
+}
+
+TEST_CASE("Y euler rotate basis vectors as expected", "[math][euler]")
+{
+    Euler e{0, 90.0f * (float)deg2rad, 0};
+    Vec4 v1{1, 0, 0, 0};
+    Vec4 result1{0, 0, -1, 0};
+    REQUIRE_THAT(e.toMatrix() * v1, Vec4ApproxMatcher(result1, 1e-5f));
+
+    Vec4 v2{0, 1, 0, 0};
+    Vec4 result2{0, 1, 0, 0};
+    REQUIRE_THAT(e.toMatrix() * v2, Vec4ApproxMatcher(result2, 1e-5f));
+
+    Vec4 v3{0, 0, 1, 0};
+    Vec4 result3{1, 0, 0, 0};
+    REQUIRE_THAT(e.toMatrix() * v3, Vec4ApproxMatcher(result3, 1e-5f));
+}
+
+TEST_CASE("Z euler rotate basis vectors as expected", "[math][euler]")
+{
+    Euler e{0, 0, 90.0f * (float)deg2rad};
+    Vec4 v1{1, 0, 0, 0};
+    Vec4 result1{0, 1, 0, 0};
+    REQUIRE_THAT(e.toMatrix() * v1, Vec4ApproxMatcher(result1, 1e-5f));
+
+    Vec4 v2{0, 1, 0, 0};
+    Vec4 result2{-1, 0, 0, 0};
+    REQUIRE_THAT(e.toMatrix() * v2, Vec4ApproxMatcher(result2, 1e-5f));
+
+    Vec4 v3{0, 0, 1, 0};
+    Vec4 result3{0, 0, 1, 0};
     REQUIRE_THAT(e.toMatrix() * v3, Vec4ApproxMatcher(result3, 1e-5f));
 }
