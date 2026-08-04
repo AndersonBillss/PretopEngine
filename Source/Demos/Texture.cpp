36ef90f76d805736714c17918791403acd6d4dd6
AndersonBillss <anderson@thebillsfamily.com>
2026-08-04 14:56:11 -0600

    Create wgpu texture

--- Source/Demos/Texture.cpp
+++ Source/Demos/Texture.cpp
@@ -13,6 +13,22 @@ struct TextureDemoData
     bool ShaderLoaded = false;
 };
 
+void CreateImage(std::vector<uint8_t> &pixels, WGPUExtent3D &size)
+{
+    pixels.resize(4 * size.width * size.height);
+    for (uint32_t i = 0; i < size.width; ++i)
+    {
+        for (uint32_t j = 0; j < size.height; ++j)
+        {
+            uint8_t *p = &pixels[4 * (j * size.width + i)];
+            p[0] = (uint8_t)i; // r
+            p[1] = (uint8_t)j; // g
+            p[2] = 128;        // b
+            p[3] = 255;        // a
+        }
+    }
+}
+
 void LoadShader(TextureDemoData *state)
 {
     Pretop::Asset::AssetManager::Status shaderLoadStatus = state->Assets->GetState(state->ShaderHandle);
@@ -43,6 +59,22 @@ void Start(Pretop::RHI::Application &application)
 
     state.ShaderHandle = state.Assets->LoadShaderModule("Does-not-exist");
 
+    std::vector<uint8_t> pixels;
+    WGPUExtent3D size{/*.width=*/256, /*.height=*/256, /*.depthOrArrayLayers=*/1};
+    CreateImage(pixels, size);
+
+    WGPUTextureDescriptor textureDesc = WGPU_TEXTURE_DESCRIPTOR_INIT;
+    textureDesc.usage = WGPUTextureUsage_TextureBinding | WGPUTextureUsage_CopyDst;
+    textureDesc.dimension = WGPUTextureDimension_2D;
+    textureDesc.size = size;
+    textureDesc.mipLevelCount = 1;
+    textureDesc.sampleCount = 1;
+    textureDesc.format = WGPUTextureFormat_RGBA8Unorm;
+    textureDesc.viewFormatCount = 0;
+    textureDesc.viewFormats = nullptr;
+
+    WGPUTexture texture = wgpuDeviceCreateTexture(application.Device->WgpuDevice, &textureDesc);
+
     application.Run(
         [&state, &application](
             double dt,
@@ -54,6 +86,9 @@ void Start(Pretop::RHI::Application &application)
                 LoadShader(&state);
             }
         });
+
+    wgpuTextureDestroy(texture);
+    wgpuTextureRelease(texture);
 }
 
 namespace Pretop::Demos
