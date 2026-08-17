9cef95a68486d4c5747090876a150a98064f3c88
AndersonBillss <anderson@thebillsfamily.com>
2026-08-17 17:52:35 -0600

    Generate asset paths for web

--- pretop_cli/codegen/gen_asset_path.py
+++ pretop_cli/codegen/gen_asset_path.py
@@ -2,13 +2,30 @@ from pathlib import Path
 
 from pretop_cli.shared.constants import ASSET_DIR, ENGINE_DIR, GEN_DIR
 
+
+def createAssetBaseDeclaration(asset_path):
+    return f'constexpr std::string_view AssetBase = "{asset_path}";\n'
+
+
+NATIVE_ASSET_PATH = f"{ENGINE_DIR}/{ASSET_DIR}"
+WEB_ASSET_PATH = ASSET_DIR
 FILE_COMMENT = "// Generate by Pretop CLI, do not touch\n"
-FILE_HEADER = "#include <iostream>\n#include <string_view>\n\n"
-ASSET_BASE_DECLARATION = f"constexpr std::string_view AssetBase = \"{ENGINE_DIR}/{ASSET_DIR}\";\n"
-FILE_CONTENTS = FILE_COMMENT + FILE_HEADER + ASSET_BASE_DECLARATION
+FILE_HEADER = (
+    '#include <iostream>\n#include <string_view>\n#include "../Core/Platform.hpp"\n\n'
+)
+FILE_CONTENTS = (
+    FILE_COMMENT
+    + FILE_HEADER
+    + "#ifdef PRETOP_PLATFORM_WEB\n"
+    + createAssetBaseDeclaration(WEB_ASSET_PATH)
+    + "#else\n"
+    + createAssetBaseDeclaration(NATIVE_ASSET_PATH)
+    + "#endif\n"
+)
+
 
 def gen_asset_path():
-    output_file = Path(GEN_DIR) / Path("AssetBase.hpp") 
+    output_file = Path(GEN_DIR) / Path("AssetBase.hpp")
     output_file.parent.mkdir(parents=True, exist_ok=True)
     with open(output_file, "w") as file:
         file.write(FILE_CONTENTS)
