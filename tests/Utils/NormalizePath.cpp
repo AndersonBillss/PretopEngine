ec8ca01e5e0e06d9390c2837dec1053fa56529eb
AndersonBillss <anderson@thebillsfamily.com>
2026-08-25 10:14:57 -0600

    Made NormalizePath tests more concise

--- tests/Utils/NormalizePath.cpp
+++ tests/Utils/NormalizePath.cpp
@@ -4,102 +4,83 @@
 
 TEST_CASE("NormalizePath returns correct values for already normalized paths", "[Utils][NormalizePath]")
 {
-    std::string path = "test/player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == path);
+    REQUIRE(Pretop::Utils::NormalizePath("test/player.png") == "test/player.png");
 }
 
 TEST_CASE("NormalizePath returns correct values for ./", "[Utils][NormalizePath]")
 {
-    std::string path = "./test/player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "test/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("./test/player.png") == "test/player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../", "[Utils][NormalizePath]")
 {
-    std::string path = "/test../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("/test../player.png") == "/player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../ with immediately preceeding /", "[Utils][NormalizePath]")
 {
-    std::string path = "/test/../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("/test/../player.png") == "/player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../ with no preceeding /", "[Utils][NormalizePath]")
 {
-    std::string path = "test/../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "player.png");
-
-    std::string path2 = "test../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path2) == "player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("test/../player.png") == "player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("test../player.png") == "player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../ with no preceeding path", "[Utils][NormalizePath]")
 {
-    std::string path = "../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../player.png") == "../player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../ with two preceeding paths", "[Utils][NormalizePath]")
 {
-    std::string path = "assets/test/../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "assets/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("assets/test/../player.png") == "assets/player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles two ../ with no preceeding path", "[Utils][NormalizePath]")
 {
-    std::string path = "../../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../../player.png") == "../../player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles three ../ with no preceeding path", "[Utils][NormalizePath]")
 {
-    std::string path = "../../../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../../../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../../../player.png") == "../../../player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles several ../", "[Utils][NormalizePath]")
 {
-    std::string path = "../test/../test/../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../test/../test/../player.png") == "../player.png");
 
-    std::string path2 = "../../test/../test../../test/../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path2) == "../../../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../../test/../test../../test/../player.png") == "../../../player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles ../ with ./", "[Utils][NormalizePath][only]")
 {
-    std::string path = "./../player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("./../player.png") == "../player.png");
 }
 
 TEST_CASE("NormalizePath Correctly resovles empty paths", "[Utils][NormalizePath][only]")
 {
-    std::string path = "./test/../";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "");
+    REQUIRE(Pretop::Utils::NormalizePath("./test/../") == "");
 }
 
 TEST_CASE("NormalizePath Correctly resovles a bunch of ./ paths", "[Utils][NormalizePath][only]")
 {
-    std::string path = "./././././";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "");
+    REQUIRE(Pretop::Utils::NormalizePath("./././././") == "");
 }
 
 TEST_CASE("NormalizePath Converts \\ to /", "[Utils][NormalizePath][only]")
 {
-    std::string path = "tests\\files\\assets\\player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "tests/files/assets/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("tests\\files\\assets\\player.png") == "tests/files/assets/player.png");
 }
 
 TEST_CASE("NormalizePath resolves a mix of \\ and /", "[Utils][NormalizePath][only]")
 {
-    std::string path = "../..\\tests\\files/../..\\assets/player.png";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "../../assets/player.png");
+    REQUIRE(Pretop::Utils::NormalizePath("../..\\tests\\files/../..\\assets/player.png") == "../../assets/player.png");
 }
 
 TEST_CASE("NormalizePath resolves a path that ends with /", "[Utils][NormalizePath][only]")
 {
-    std::string path = "test/";
-    REQUIRE(Pretop::Utils::NormalizePath(path) == "test");
+    REQUIRE(Pretop::Utils::NormalizePath("test/") == "test");
 }
